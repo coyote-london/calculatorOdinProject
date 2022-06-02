9cf77f5519186803436b238cbd1f720ecf71227f
Reid Jernigan <reidlondonjernigan@gmail.com>
2022-06-02 13:48:44 -0500

    Rewrite logic for calculator in OOP to clean up code and practice syntax

--- script.js
+++ script.js
@@ -1,103 +1,157 @@
-const operationsSupported = ['+', '-', '*', '/']
+//Rewrite the logic on storing the numbers 
+//and running equations
+//Maybe work the logic to display the numbers first?
+//may make testing easier that way if you get frusturated
 
-function add(num1, num2) {
-    return num1 + num2;
-}
-
-function subtract(num1, num2) {
-    return num1 - num2;
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    return num1 / num2;
-}
-function operate(num1, operand, num2) {
-    if (operand == '+' || operand == '-') {
-        return operand == '+' ? add(num1, num2) : subtract(num1, num2);
+let calculator = {
+    input: ''
+    ,
+    numberFirst: undefined
+    ,
+    numberSecond: undefined
+    ,
+    operatorStored: undefined
+    ,
+    operatorUsed: undefined
+    ,
+    storeInput(num){
+        if (this.operatorStored == undefined) this.numberFirst = undefined; 
+        this.input = this.input + num;
+        console.log(this.input);
     }
-    else if (operand == '*' || operand == '/') {
-         return operand == '*' ? multiply(num1, num2) : divide(num1, num2);
-    } 
-    else {
-        return 'This is not supported';
+    ,
+    storeNumber(){
+        this.numberFirst == undefined 
+        ?
+        this.numberFirst = parseInt(this.input) 
+        :
+        this.numberSecond = parseInt(this.input);
+        this.input = '';
     }
-}
-let input=[]
-let value1;
-let value2;
-function storeInput(num) {
-    if(isNaN(num)) {
-        isNaN(value1) ? value1 = parseInt(input.join('').toString()) : value2 = parseInt(input.join('').toString());
-        input =[];
+    ,
+    storeOperator(symbol) {
+        if (this.operatorStored == undefined) { 
+            this.operatorStored = symbol;
+            this.storeNumber();
+            console.log(this.operatorStored)
+        }
+        else {
+            this.operatorUsed = this.operatorStored;
+            this.runOperation(this.operatorUsed);
+            symbol == '=' ?
+                this.operatorStored = undefined
+                :
+                this.operatorStored = symbol;
+            console.log(this.operatorStored);
+        }
     }
-    else input.push(num);
+    ,
+    runOperation(operator) {
+        this.storeNumber(this.input)
+        console.log(this.numberFirst + ' ' + this.operatorUsed + ' ' + this.numberSecond); 
+        switch(operator) {
+            case '+':
+                this.add(this.numberFirst, this.numberSecond);
+                break;
+            case '-':
+                this.subtract(this.numberFirst, this.numberSecond);
+                break;
+            case '*':
+                this.multiply(this.numberFirst, this.numberSecond);
+                break;
+            case '/':
+                this.divide(this.numberFirst, this.numberSecond);
+                break;
+        }
+    }
+    ,
+    add(num1, num2) {
+        this.numberFirst = num1 + num2;
+        console.log(this.numberFirst)
+        this.input = '';
+        
+    }
+    ,
+    subtract(num1, num2) {
+        this.numberFirst = num1 - num2;
+        console.log(this.numberFirst)
+        this.input = '';
+    }
+    ,
+    divide(num1, num2) {
+        this.numberFirst = num1 / num2;
+        console.log(this.numberFirst)
+        this.input = '';
+    }
+    ,
+    multiply(num1, num2) {
+        this.numberFirst = num1 * num2;
+        console.log(this.numberFirst)
+        this.input = '';
+    }
 }
 
 document.getElementById('button7').addEventListener('click', e => {
     e.stopPropagation;
-    storeInput(7);
+    calculator.storeInput(7)
 });
 document.getElementById('button8').addEventListener('click', e => {
     e.stopPropagation;
-    storeInput(8);
+    calculator.storeInput(8);
 });
 document.getElementById('button9').addEventListener('click', e => {
     e.stopPropagation;
-    storeInput(9);
+    calculator.storeInput(9);
 });
 document.getElementById('button/').addEventListener('click', e => {
     e.stopPropagation;
-    storeInput('/');
+    calculator.storeOperator('/');
 });
 document.getElementById('button4').addEventListener('click', e => {
     e.stopPropagation;
-    storeInput(4);
+    calculator.storeInput(4);
 });
 document.getElementById('button5').addEventListener('click', e => {
     e.stopPropagation;
-    storeInput(5);
+    calculator.storeInput(5);
 });
 document.getElementById('button6').addEventListener('click', e => {
     e.stopPropagation;
-    storeInput(6);
+    calculator.storeInput(6);
 });
 document.getElementById('button*').addEventListener('click', e => {
     e.stopPropagation;
-    storeInput('*');
+    calculator.storeOperator('*');
 });
 document.getElementById('button1').addEventListener('click', e => {
     e.stopPropagation;
-    storeInput(1);
+    calculator.storeInput(1);
 });
 document.getElementById('button2').addEventListener('click', e => {
     e.stopPropagation;
-    storeInput(2);
+    calculator.storeInput(2);
 });
 document.getElementById('button3').addEventListener('click', e => {
     e.stopPropagation;
-    storeInput(3);
+    calculator.storeInput(3);
 });
 document.getElementById('button-').addEventListener('click', e => {
     e.stopPropagation;
-    storeInput('-');
+    calculator.storeOperator('-');
 });
 document.getElementById('button0').addEventListener('click', e => {
     e.stopPropagation;
-    storeInput(0);
+    calculator.storeInput(0);
 });
 document.getElementById('button.').addEventListener('click', e => {
     e.stopPropagation;
-    storeInput('.');
+    calculator.storeOperator('.');
 });
 document.getElementById('button=').addEventListener('click', e => {
     e.stopPropagation;
-    storeInput('=');
+    calculator.storeOperator('=');
 });
 document.getElementById('button+').addEventListener('click', e => {
     e.stopPropagation;
-    storeInput('+');
+    calculator.storeOperator('+');
 });
